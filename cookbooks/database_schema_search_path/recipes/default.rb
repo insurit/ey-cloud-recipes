22a06d3da83bd74c26ce046e23c69cfe1d37d259
Riley Stonehouse <insuritsolutions@gmail.com>
2015-10-08 00:58:00 +0000

    Updated search path correctly and added pdftk

--- cookbooks/database_schema_search_path/recipes/default.rb
+++ cookbooks/database_schema_search_path/recipes/default.rb
@@ -9,7 +9,7 @@ node[:applications].each do |app_name, _|
   # database.yml
   execute "update-database-yml-schema-search-path-for-#{app_name}" do
     db_yaml_file = "/data/#{app_name}/shared/config/database.yml"
-    command "sed -i '/reconnect: true/a  \ \ schema_search_path: \"public,extensions\"' #{db_yaml_file}"
+    command "sed -i '/reconnect: true/a  \\ \\ schema_search_path: \"public,extensions\"' #{db_yaml_file}"
     action :run
   end
 
