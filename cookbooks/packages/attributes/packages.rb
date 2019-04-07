29dbd39a91d054f2e610a1b49201159950b26ea3
Riley Stonehouse <riley.stonehouse@gmail.com>
2019-04-07 19:48:28 +0000

    Updated wkhtmltopdf version

--- cookbooks/packages/attributes/packages.rb
+++ cookbooks/packages/attributes/packages.rb
@@ -1,11 +1,11 @@
 # Specify packages and version numbers to be installed here
 #
-# Search for packages on instances using: eix <package name> 
+# Search for packages on instances using: eix <package name>
 # Or go to the dashboard and edit the packages for an application to view *unmasked* packages
 # Note that the dashboard view will not list masked packages
 #
 # Examples below:
 
 default[:packages] = [#{:name => "app-misc/wkhtmltopdf-bin", :version => "0.12.0_beta5"},
-                      {:name => "media-gfx/wkhtmltopdf-bin", :version => "0.12.2.1"},
+                      {:name => "media-gfx/wkhtmltopdf-bin", :version => "0.12.4"},
                       {:name => "app-text/pdftk", :version => "2.02"}]
